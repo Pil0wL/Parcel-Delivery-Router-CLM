744a17c383e9ef95f897214a5b7936250b7d017c
Pil0wL <cadmonjaslibetario@gmail.com>
2025-11-09 00:51:20 +0800

    added the parcel class

--- model/Parcel.java
+++ model/Parcel.java
@@ -7,5 +7,18 @@ package parceldeliveryproject.model;
 
 
 public class Parcel {
-    public int bruh = 0;
+    public final String ID;
+    public final String Name;
+    public final short ZIP;
+    public final float Weight; // in kilograms
+    public final boolean Fragile;
+    public boolean InTransit = false;
+    
+    public Parcel(String Name, short ZIP, float Weight, boolean Fragile) {
+        ID = Long.toString(System.nanoTime());;
+        this.Name = Name;
+        this.ZIP = ZIP;
+        this.Weight = Weight;
+        this.Fragile = Fragile;
+    }
 }
